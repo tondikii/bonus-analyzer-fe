13563f0e8f72e82e3569079df35fac5934c75912
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2024-09-11 14:02:36 +0700

    home page completed

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,16 @@
 <template>
-  <HelloWorld />
+  <!-- <HelloWorld /> -->
+  <div class="d-flex flex-column w-100 align-center justify-center">
+    <v-img class="mb-4 w-50 mt-16" src="@/assets/Working-on-a-project.svg" />
+    <h3 class="">Selamat bekerja {{ session?.name }}</h3>
+
+  </div>
 </template>
 
 <script setup>
+import { useSessionStore } from '@/stores/app';
+
+
+const session = useSessionStore()
 //
 </script>
